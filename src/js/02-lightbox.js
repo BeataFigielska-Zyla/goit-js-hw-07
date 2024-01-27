0fe8a089d4b9709b64d644e882816ead76bbc429
BeataFigielska-Zyla <beata.figielska@gmail.com>
2024-01-27 21:51:03 +0100

    change

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -10,20 +10,21 @@ const gallery = document.querySelector(".gallery");
 galleryItems.forEach((item) => {
   const { original, preview, description } = item;
 
-  const li = document.createElement("li");
+const li = document.createElement("li");
   li.classList.add("gallery__item");
 
-  const a = document.createElement("a");
-  a.classList.add("gallery__link");
-  a.href = original;
+  const link = document.createElement("a");
+  link.classList.add("gallery__link");
+  link.href = original;
 
   const img = document.createElement("img");
   img.classList.add("gallery__image");
   img.src = preview;
+  img.dataset.source = original;
   img.alt = description;
 
-  a.appendChild(img);
-  li.appendChild(a);
+  link.appendChild(img);
+  li.appendChild(link);
   gallery.appendChild(li);
 });
 
@@ -33,5 +34,8 @@ document.addEventListener("DOMContentLoaded", () => {
     captionsData: "alt",
     captionDelay: 250,
     captionsDataAlt: "Image description",
+    
   });
 });
+
+
